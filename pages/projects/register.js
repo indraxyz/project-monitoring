db64703df186b55074fc2c38a3634cacca393c48
indracahyae <indracahyae@gmail.com>
2024-06-02 23:28:09 +0700

    approval✅

--- pages/projects/register.js
+++ pages/projects/register.js
@@ -5,13 +5,21 @@ const Projects = () => {
     <Layout>
       <span>Projects Register Page</span>
 
+      {/* CARI + FILTER */}
+      {/* CREATE, DELETES */}
+
+      {/* TABLE: ALL, DETAIL, EDIT, DELETE */}
+
       <p>
         Lorem ipsum, atau ringkasnya lipsum, adalah teks standar yang
         ditempatkan untuk mendemostrasikan elemen grafis atau presentasi visual
         seperti font, tipografi, dan tata letak
       </p>
+
+      {/* dialog DETAIL project */}
     </Layout>
   );
 };
 
 export default Projects;
+//list(table), detail, crud, cari + filter
